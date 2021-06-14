66437e418a9bf97fad55a54695de1d97128dbbfe
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-06-14 16:46:46 +0530

    Add project files.

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public int GetSum(int a, int b)
+        {
+            new model()
+            {
+                Num1 = 1,
+                Num2 = 2
+            };
+            var s = a + b;
+            return s;
+        }
+    }
+}
---
+++ WebApplication1/Model/model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class model
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+}
